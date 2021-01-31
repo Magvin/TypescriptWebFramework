7cb839c8d5ccc39d0f7e1987c334a19e42ced6cd
Magvin <zans.litinskis@gmail.com>
2021-01-31 17:48:06 +0000

    JSON server

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { User }from './models/User';
 const newUser = new User({name: 'Albert', age: 2});
 
 newUser.on('change',()=> { 
-});
-newUser.on('change',()=> { 
+    console.log('works')
 });
 
-console.log(newUser);
+
+newUser.trigger('change');
